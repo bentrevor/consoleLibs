429fab6c73801dd7b195f5a45b25b9195d0cd88b
Ben Trevor <btrevor@8thlight.com>
2014-09-10 18:37:59 -0500

    Use lodash instead of underscore

--- consoleLibs.js
+++ consoleLibs.js
@@ -4,9 +4,10 @@ var jquery_tag = document.createElement("script");
 jquery_tag.type = "text/javascript";
 jquery_tag.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js";
 
-var underscore_tag = document.createElement("script");
-underscore_tag.type = "text/javascript";
-underscore_tag.src = "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js";
+var lodash_tag = document.createElement("script");
+lodash_tag.type = "text/javascript";
+lodash_tag.src = "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js"
+
 
 head.appendChild(jquery_tag);
-head.appendChild(underscore_tag);
+head.appendChild(lodash_tag);
